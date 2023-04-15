0dfcf9f21537df62587b2f00f2ca7b2c6767ff3c
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2023-04-15 19:31:42 -0300

    syle: change enviroment info badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,32 @@
 import Image from 'next/image';
-import { Home as HomeIcon, AlertCircleIcon, Search, Library, PlusSquare, Heart, ChevronLeft, ChevronRight, Play } from 'lucide-react';
+import { Home as HomeIcon, AlertCircleIcon, Search, Library, PlusSquare, Heart, ChevronLeft, ChevronRight, Play, Repeat, SkipForward, Shuffle, SkipBack, Mic2, LayoutList, Laptop2, Volume, Maximize2 } from 'lucide-react';
+import { Sidebar } from '../components/Sidebar';
+import { Footer } from '../components/Footer';
 
 export default function Home() {
   return (
     <div className='h-screen flex flex-col'>
-      <div className='flex items-center justify-center bg-yellow-300 text-zinc-800 mb-4'>
-        <span className='flex items-center gap-4 p-2 text-xs'>
-          <AlertCircleIcon className='h-4 w-4' />
-          Ambiente de desenvolvimento
-        </span>
-      </div>
       <div className='flex flex-1'>
-        <aside className='w-72 bg-zinc-950 p-6'>
-          <nav className='space-y-5'>
-            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
-              <HomeIcon />
-              Home
-            </a>
-            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
-              <Search />
-              Search
-            </a>
-            <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
-              <Library />
-              Your Library
-            </a>
 
-            <div className='space-y-5 pt-6'>
-              <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
-                <PlusSquare />
-                Create Playlist
-              </a>
-              <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
-                <Heart />
-                Liked Songs
-              </a>
-            </div>
-          </nav>
+        <Sidebar />
 
-          <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Nirvana</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Skrillex</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Pearl Jam</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Matuê</a>
-            <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Gorillaz</a>
-          </nav>
-        </aside>
         <main className='flex-1 p-6'>
-          <div className='flex items-center gap-4'>
-            <button className='rounded-full bg-black/40 p-1'>
-              <ChevronLeft />
-            </button>
-            <button className='rounded-full bg-black/40 p-1'>
-              <ChevronRight />
-            </button>
+          <div className='flex items-center justify-between'>
+            <div className='flex items-center gap-4'>
+              <button className='rounded-full bg-black/40 p-1'>
+                <ChevronLeft />
+              </button>
+              <button className='rounded-full bg-black/40 p-1'>
+                <ChevronRight />
+              </button>
+            </div>
+
+            <div className='flex items-center justify-center '>
+              <span className='flex items-center gap-4 p-2 text-xs rounded-full text-zinc-900 font-semibold border-solid border-1 bg-yellow-300'>
+                <AlertCircleIcon />
+                Ambiente de desenvolvimento
+              </span>
+            </div>
           </div>
 
           <h1 className='font-semibold text-3xl mt-10'>Good Afternoon</h1>
@@ -105,7 +78,7 @@ export default function Home() {
 
           <h2 className='font-semibold text-2xl mt-10'>Made for Kelvin Carvalho</h2>
 
-          <div className='grid grid-cols-8 gap-4 mt-4'>
+          <div className='grid grid-cols-5 gap-4 mt-4'>
             <a href="" className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
               <Image src='/album.jpg' className='w-full' width={120} height={120} alt='capa do album do Skrillex' />
               <strong className='font-semibold'>Daily Mix 1</strong>
@@ -134,15 +107,9 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className='bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-bettween'>
-        <div className='flex items-center gap-2'>
-          <Image src='/album.jpg' className='w-full' width={56} height={56} alt='capa do album do Skrillex' />
-          <div className='flex items-flex flex-col gap-3'>
-            <strong className='font-normal'>Bangarang</strong>
-            <span className='text-xs text-zinc-400'>Skrillex</span>
-          </div>
-        </div>
-      </footer>
+
+      <Footer />
+
     </div>
   )
 }
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,41 @@
+import { Heart, HomeIcon, Library, PlusSquare, Search } from "lucide-react";
+
+export function Sidebar() {
+  return (
+    <aside className='w-72 bg-zinc-950 p-6'>
+      <nav className='space-y-5'>
+        <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+          <HomeIcon />
+          Home
+        </a>
+        <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+          <Search />
+          Search
+        </a>
+        <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+          <Library />
+          Your Library
+        </a>
+
+        <div className='space-y-5 pt-6'>
+          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+            <PlusSquare />
+            Create Playlist
+          </a>
+          <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
+            <Heart />
+            Liked Songs
+          </a>
+        </div>
+      </nav>
+
+      <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Nirvana</a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Skrillex</a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Pearl Jam</a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Matuê</a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Gorillaz</a>
+      </nav>
+    </aside>
+  )
+}
