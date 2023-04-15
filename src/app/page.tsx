12797c29b2738c30e64e3ce67a5bfcfd99e7f2ec
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2023-04-15 12:32:01 -0300

    chore: add action buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
-import { Home as HomeIcon, Search, Library, PlusSquare, Heart, ChevronLeft, ChevronRight, Play, CheckCircleIcon } from 'lucide-react';
+import { Home as HomeIcon, Search, Library, PlusSquare, Heart, ChevronLeft, ChevronRight, Play, CheckCircleIcon, Shuffle, SkipBack, SkipForward, Repeat } from 'lucide-react';
 
 export default function Home() {
   return (
     <div className='h-screen flex flex-col'>
+
       <div className='flex items-center justify-center bg-green-600 text-zinc-50 mb-4'>
         <span className='flex items-center gap-4 p-2 text-xs'>
           <CheckCircleIcon className='h-4 w-4' />
           Ambiente de produção
         </span>
       </div>
+
       <div className='flex flex-1'>
         <aside className='w-72 bg-zinc-950 p-6'>
           <nav className='space-y-5'>
@@ -46,6 +48,7 @@ export default function Home() {
             <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Gorillaz</a>
           </nav>
         </aside>
+
         <main className='flex-1 p-6'>
           <div className='flex items-center gap-4'>
             <button className='rounded-full bg-black/40 p-1'>
@@ -134,14 +137,36 @@ export default function Home() {
           </div>
         </main>
       </div>
-      <footer className='bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-bettween'>
+
+      <footer className='bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between'>
         <div className='flex items-center gap-2'>
           <Image src='/album.jpg' className='w-full' width={56} height={56} alt='capa do album do Skrillex' />
-          <div className='flex items-flex flex-col gap-3'>
+          <div className='flex items-flex flex-col'>
             <strong className='font-normal'>Bangarang</strong>
             <span className='text-xs text-zinc-400'>Skrillex</span>
           </div>
         </div>
+
+        <div className='flex flex-col items-center gap-2'>
+          <div className='flex items-center gap-6'>
+            <Shuffle size={20} className='text-zinc-200' />
+            <SkipBack size={20} className='text-zinc-200' />
+
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
+              <Play />
+            </button>
+
+            <SkipForward size={20} className='text-zinc-200' />
+            <Repeat size={20} className='text-zinc-200' />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <span className='text-xs text-zinc-400'>0:30</span>
+            <span className='text-xs text-zinc-400'>3:45</span>
+          </div>
+        </div>
+
+        <div></div>
       </footer>
     </div>
   )
