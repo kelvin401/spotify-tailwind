3f1028b2e2e66eb3b47619c2234743e3768cbc72
Kelvin Carvalho <kelvin.nirvana@gmail.com>
2023-04-15 22:38:56 -0300

    style: add link to repository

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { Heart, HomeIcon, Library, PlusSquare, Search } from "lucide-react";
+import { Heart, HomeIcon, Library, PlusSquare, Search, ExternalLink } from "lucide-react";
 
 export function Sidebar() {
   return (
@@ -35,6 +35,13 @@ export function Sidebar() {
         <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Pearl Jam</a>
         <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Matuê</a>
         <a href="" className="text-sm text-zinc-400 hover:text-zinc-100">Gorillaz</a>
+      </nav>
+
+      <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
+        <a href='https://github.com/kelvin401/spotify-tailwind' target="_blank" className="flex gap-2 items-center justify-center text-sm text-zinc-400 hover:text-zinc-100">
+          View on Github
+          <ExternalLink size={16} />
+        </a>
       </nav>
     </aside>
   )
